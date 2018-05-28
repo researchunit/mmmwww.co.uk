e079dd06cad2e9801bae82d28a062b3ce3a616fd
Peter Bruce <peter@researchunit.co.uk>
2018-05-28 17:27:07 +0100

    Added Slideshow

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,54 @@
+// Slider
+
+var utils = window.fizzyUIUtils;
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  selectedAttraction: 1,
+  friction: 1,
+  draggable: false,
+  prevNextButtons: false,
+  pageDots: false
+});
+
+// elements
+var cellsButtonGroup = document.querySelector('.button-group--cells');
+var cellsButtons = utils.makeArray( cellsButtonGroup.children );
+
+// update buttons on select
+flkty.on( 'select', function() {
+  var previousSelectedButton = cellsButtonGroup.querySelector('.is-selected');
+  var selectedButton = cellsButtonGroup.children[ flkty.selectedIndex ];
+  previousSelectedButton.classList.remove('is-selected');
+  selectedButton.classList.add('is-selected');
+});
+
+// cell select
+cellsButtonGroup.addEventListener( 'click', function( event ) {
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = cellsButtons.indexOf( event.target );
+  flkty.select( index );
+});
+// previous
+var previousButton = document.querySelector('.button--previous');
+previousButton.addEventListener( 'click', function() {
+  flkty.previous();
+});
+// next
+var nextButton = document.querySelector('.button--next');
+nextButton.addEventListener( 'click', function() {
+  flkty.next();
+});
+
+
+
+
 // modal
 
 var modal = document.getElementById("custom_modal");
